ffacc42d91280ad2230bf1bb8184d277c7ccb259
yadav <yadav@DESKTOP-R9R1TK9>
2021-07-11 23:14:32 +0530

    second commit
    if statement

---
+++ JavaBasic/src/control_statement/If_statement.java
@@ -0,0 +1,12 @@
+package control_statement;
+
+public class If_statement {
+
+	public static void main(String[] args) {
+		
+		int x=10,y= 20;
+		
+		if(x<y)
+			System.out.println("x is smaller than y");
+	}
+}
